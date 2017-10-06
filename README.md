748ec40965dcc4174ea26db01533ce2e36130c21
tide_inokuchi <inokuchi@tidedev.jp>
2017-10-07 06:11:28 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# t
+
+[![Build Status][travis-badge]][travis-link]
+[![Slack Room][slack-badge]][slack-link]
+
+super simple tmux attacher
+
+## Install
+
+With [fisherman]
+
+```
+fisher inokuc/t
+```
+
+## Usage
+
+```fish
+t
+```
+
+[travis-link]: https://travis-ci.org/inokuc/t
+[travis-badge]: https://img.shields.io/travis/inokuc/t.svg
+[slack-link]: https://fisherman-wharf.herokuapp.com
+[slack-badge]: https://fisherman-wharf.herokuapp.com/badge.svg
+[fisherman]: https://github.com/fisherman/fisherman
---
+++ functions/t.fish
@@ -0,0 +1,53 @@
+function t -d "tmux attacher"
+  # tmux is required.
+  if not test ( which tmux )
+    echo 'Error: tmux command not found' 2>&1
+    return 1
+  end
+
+  # screen is running, or tmux is already running.
+  if test $TERM = "screen-256color"; or test $TMUX
+    echo (set_color $fish_color_comment)"tmux already attached!"(set_color normal)
+    return 0
+  end
+
+  # in ssh connection.
+  if test $SSH_CONNECTION
+    echo "SSH_CONNECTION IS DETECTED. ABORT."
+    return 0
+  end
+
+  set detached_session_num ( tmux list-sessions | ag ']$' | wc -l )
+
+  if test $detached_session_num -eq 0
+    tmux new-session
+  else
+    # detached session exists
+    tmux list-sessions
+    read -p 'echo (set_color $fish_color_comment)"Tmux: attach? y/N/num > "(set_color normal)' reply
+
+    if string match -r '^[Yy]$' $reply; or test $reply = ''
+      set detached_session_ids ( tmux list-sessions | ag ']$' | string match -r '^[0-9]+' )
+      tmux attach -t "$detached_session_ids[1]"
+      if [ $status -eq 0 ]
+        echo "(tmux -V) attached session"
+        return 0
+      end
+    else if string match -r '^[0-9]+$' $reply
+      tmux attach -t "$reply"
+      if [ $status -eq 0 ]
+        echo "(tmux -V) attached session"
+        return 0
+      end
+    else if string match -r '[Nn]$' $reply
+      tmux new-session
+      if [ $status -eq 0 ]
+        echo "(tmux -V) create new session"
+        return 0
+      end
+    else
+      echo "Invalid input. Abort."
+      return 0
+    end
+  end
+end
